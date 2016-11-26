lucasec/mu

commit 9a3ff05192c0e4a5a6fe8d3abe4d1b1d0e796bbc

mu4e: handle overflow for plaintext-ratio-heuristic
Handle overflow when multiplying
mu4e-view-html-plaintext-ratio-heuristic by most-positive-fixnum.

mu4e/mu4e-message.el
@@ -170,6 +170,9 @@ unless PREFER-HTML is non-nil."
   (setq mu4e~message-body-html nil) ;; default
   (let* ((txt (mu4e-message-field msg :body-txt))
 	  (html (mu4e-message-field msg :body-html))
+	  (txtlen (length txt))
+	  (txtlimit (* mu4e-view-html-plaintext-ratio-heuristic txtlen))
+	  (txtlimit (if (>= txtlimit 0) txtlimit most-positive-fixnum)) ;; overflow
 	  (body
 	    (cond
 	      ;; does it look like some text? ie., if the text part is more than
@@ -177,10 +180,8 @@ unless PREFER-HTML is non-nil."
 	      ;; html part, it should't be used
 	      ;; This is an heuristic to guard against 'This messages requires
 	      ;; html' text bodies.
-	      ((and (> (* mu4e-view-html-plaintext-ratio-heuristic
-                      (length txt)) (length html))
-		 ;; use html if it's prefered, unless there is no html
-		 (or (not prefer-html) (not html)))
+	      ((and (> txtlen 0)
+		 (or (> txtlimit (length html)) (not prefer-html)))
 		txt)
 	      ;; otherwise, it there some html?
 	      (html

commit 8a2cf7bd7904191beaf7530d3fd216608b791ded

mu4e: Honor mu4e-view-prefer-html, but allow toggling off.
This fixes issues introduced by
9a3ff05192c0e4a5a6fe8d3abe4d1b1d0e796bbc, where the text version would
always be preferred if the mu4e-view-html-plaintext-ratio-heuristic
check passed. Mu4e now properly honors mu4e-view-prefer-html.

The addition of the mu4e-view-force-text variable allows users to
manually select the text version of a message, again overruling the
heuristic check. The H keybinding (mu4e-view-toggle-html) makes use of
this variable, ensuring the key reliably works regardless of the
default heuristics and preferences.

mu4e/mu4e-message.el
@@ -64,6 +64,10 @@ is always used."
   :type 'boolean
   :group 'mu4e-view)
 
+(defvar mu4e-view-force-text nil
+  "Whether to force messages to always render their text version.
+If the message has no text version, an empty body will be rendered.")
+
 (defcustom mu4e-view-html-plaintext-ratio-heuristic 5
   "Ratio between the length of the html and the plain text part
 below which mu4e will consider the plain text part to be 'This
@@ -180,8 +184,12 @@ unless PREFER-HTML is non-nil."
 	      ;; html part, it should't be used
 	      ;; This is an heuristic to guard against 'This messages requires
 	      ;; html' text bodies.
-	      ((and (> txtlen 0)
-		 (or (> txtlimit (length html)) (not prefer-html)))
+         ((or
+           (zerop (length html))
+           (and (> txtlen 0)
+                (or
+                 mu4e-view-force-text
+                 (and (> txtlimit (length html)) (not prefer-html)))))
 		txt)
 	      ;; otherwise, it there some html?
 	      (html

mu4e/mu4e-view.el
@@ -972,8 +972,9 @@ the new docid. Otherwise, return nil."
 (defun mu4e-view-toggle-html ()
   "Toggle html-display of the message body (if any)."
   (interactive)
-  (let ((mu4e-view-prefer-html (not mu4e~message-body-html))) 
-    (mu4e-view-refresh))) 
+  (let ((mu4e-view-prefer-html (not mu4e~message-body-html))
+        (mu4e-view-force-text mu4e~message-body-html))
+    (mu4e-view-refresh)))
 
 (defun mu4e-view-refresh ()
   "Redisplay the current message."